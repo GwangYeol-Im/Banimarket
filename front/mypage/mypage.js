d1d811e39dd6457a5db9c3e8b7ed2c6681df1702
Kyle <mkitigy@gmail.com>
2020-11-02 19:32:03 +0900

    Add tabs in mypage

--- front/mypage/mypage.js
+++ front/mypage/mypage.js
@@ -1,19 +1,42 @@
 "use strict!";
 
 const EditProfile = document.querySelector(".ProfileCard__ProfileEdit");
-const EditButton = document.querySelector(".EditModal__EditButton");
 const CloseButton = document.querySelector(".EditModal__CloseButton");
+const ProfileTabLink = document.querySelector(".tablink_1");
+const PasswordTabLink = document.querySelector(".tablink_2");
+const Tabs = document.querySelectorAll(".tabs");
+const ProfileEditBtn = document.querySelector(".EditModal__ProfileEditBtn");
+const PasswordEditBtn = document.querySelector(".EditModal__PasswordEditBtn");
 const Modal = document.querySelector(".MyPage__EditModal");
 const DarkBg = document.querySelector(".Dark__BackGround");
 
-EditProfile.addEventListener("click", function () {
+EditProfile.addEventListener("click", openModal);
+ProfileEditBtn.addEventListener("click", closeModal);
+PasswordEditBtn.addEventListener("click", closeModal);
+CloseButton.addEventListener("click", closeModal);
+
+function toggleTab(event, num) {
+  event.preventDefault();
+  for (let i = 0; i < Tabs.length; i++)
+    Tabs[i].style.display = "none";
+  let tabLink1 = document.querySelector(`.tablink_1`);
+  let tabLink2 = document.querySelector(`.tablink_2`);
+  let tab = document.querySelector(`.tab_${num}`);
+  tab.style.display = "block";
+  if (num == 1){
+    tabLink1.style.color = "black";
+    tabLink2.style.color = "grey";
+  } else {
+    tabLink1.style.color = "grey";
+    tabLink2.style.color = "black";
+  }
+}
+
+function openModal() {
   if (Modal.style.opacity > 0) return;
   fadeIn(DarkBg.style, 0.3);
   fadeIn(Modal.style, 0.9);
-});
-
-EditButton.addEventListener("click", closeModal);
-CloseButton.addEventListener("click", closeModal);
+}
 
 function closeModal(event) {
   event.preventDefault();
